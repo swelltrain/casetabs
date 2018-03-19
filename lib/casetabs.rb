a24deafa310028b67c36ed3b0c21a3c205233ebe
Stephen Philp <swelltrain@gmail.com>
2018-03-19 12:47:21 -0700

    Casetabs.zero_count
      counts the number of zeros
      when the string has no zeros
        returns 0

--- lib/casetabs.rb
+++ lib/casetabs.rb
@@ -3,5 +3,27 @@ require "casetabs/string_to_hash"
 require "casetabs/hash_with_methods"
 
 module Casetabs
+  def self.zero_count(n)
+    # Time Complexity: Our worst case string would be 10101...repeated
+    # This should have a worst case run time of O(n log n)
+    # Assuming Regex in ruby is optimized and  negligible
 
+    # Space Complexity: our worst case string would be 100...001, resulting in matched being a complete copy of the string
+    # O(n**2)
+    # Assuming string[] syntax is working on the same string in memory and not creating a new one
+
+    max = 0
+    return max if n.nil?
+    string = n.to_s(2)
+    start_index = 0
+
+    while match_data = string[start_index..-1].match(/10+1/) do
+      matched = match_data[0]
+      start_index += string[start_index..-1].index(matched) + matched.length - 1
+      number_of_zeroes = matched.length - 2
+      max = number_of_zeroes > max ? number_of_zeroes : max
+    end
+
+    max
+  end
 end
--- spec/casetabs_spec.rb
+++ spec/casetabs_spec.rb
@@ -25,3 +25,19 @@ RSpec.describe Casetabs::StringToHash, '#keys' do
     expect(Casetabs::StringToHash.new("{ key:[ [value_1, value_2],[value_3, value4] ], 5:10:00AM ] }").keys).to eq([" key:", " 5:"])
   end
 end
+
+RSpec.describe Casetabs, '.zero_count' do
+  context 'when the string has no zeros' do
+    it 'returns 0' do
+      expect(Casetabs.zero_count(7)).to eq(0)
+    end
+  end
+
+  it 'counts the number of zeros' do
+    expect(Casetabs.zero_count(9)).to eq(2)
+    expect(Casetabs.zero_count(1041)).to eq(5)
+    expect(Casetabs.zero_count(21)).to eq(1)
+  end
+end
+
+
